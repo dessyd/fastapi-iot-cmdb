a0aab3e2860c9d8b6c11119a1976b30de35a6a88
Dominique Dessy <reprendre.mezzo_0j@icloud.com>
2024-07-10 19:14:30 +0200

    Adding DocStrings

--- app/routers/locations.py
+++ app/routers/locations.py
@@ -12,24 +12,35 @@ router = APIRouter(prefix="/locations", tags=["Locations"])
 
 @router.get("/", response_model=List[schemas.LocationOut])
 async def get_all_locations(db: Session = Depends(get_db)):
+    """
+    Returns all known locations
+    """
     locations = db.query(models.Location).all()
     return locations
 
 
 @router.get("/{id}", response_model=schemas.LocationOut)
 async def get_one_location(
-    id: Annotated[int, Path(title="The ID of the location to get")], db: Session = Depends(get_db)
+    id: Annotated[int, Path(description="The ID of the location to get")],
+    db: Session = Depends(get_db),
 ):
+    """
+    Returns a specific location identified by its id
+    """
     location = db.query(models.Location).filter(models.Location.id == id).first()
     if location is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Location with id {id} not found"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Location with id {id} not found",
         )
     return location
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.LocationOut)
 async def create_one_location(location: schemas.LocationCreate, db: Session = Depends(get_db)):
+    """
+    Create a new location
+    """
     new_location = models.Location(**location.model_dump())
     db.add(new_location)
     db.commit()
@@ -39,13 +50,17 @@ async def create_one_location(location: schemas.LocationCreate, db: Session = De
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_one_location(id: int, db: Session = Depends(get_db)):
+async def delete_one_location(
+    id: Annotated[int, Path(description="The ID of the location to delete")],
+    db: Session = Depends(get_db),
+):
     location = db.query(models.Location).filter(models.Location.id == id)
     first_location = location.first()
 
     if first_location is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Location with id: {id} does not exeist"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Location with id: {id} does not exeist",
         )
 
     location.delete(synchronize_session=False)
@@ -55,13 +70,18 @@ async def delete_one_location(id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/{id}", response_model=schemas.LocationOut)
-def update_one_location(id: int, location: schemas.LocationUpdate, db: Session = Depends(get_db)):
+def update_one_location(
+    id: Annotated[int, Path(description="The ID of the location to update")],
+    location: schemas.LocationUpdate,
+    db: Session = Depends(get_db),
+):
     location_query = db.query(models.Location).filter(models.Location.id == id)
     first_location = location_query.first()
 
     if first_location is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"location with id: {id} does not exist"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"location with id: {id} does not exist",
         )
 
     location_query.update(jsonable_encoder(location), synchronize_session=False)
